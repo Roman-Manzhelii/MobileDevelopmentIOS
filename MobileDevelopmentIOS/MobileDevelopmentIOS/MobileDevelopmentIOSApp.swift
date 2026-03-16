20f770d7024718e764287f8dbc5aa2dec8a7f908
Roman Manzhelii <D00264678@student.dkit.ie>
2026-03-16 11:35:35 +0000

    initial commit

---
+++ MobileDevelopmentIOS/MobileDevelopmentIOS/MobileDevelopmentIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileDevelopmentIOSApp.swift
+//  MobileDevelopmentIOS
+//
+//  Created by Student on 16/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct MobileDevelopmentIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
