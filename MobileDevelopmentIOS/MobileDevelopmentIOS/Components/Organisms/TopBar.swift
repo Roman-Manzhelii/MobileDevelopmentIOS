0cecb053fa13c1734418e3ac2c6978b07f9c89da
Robert Nugent <seanmooney1996@gmail.com>
2026-04-13 12:05:40 +0100

    update color systems

--- MobileDevelopmentIOS/MobileDevelopmentIOS/Components/Organisms/TopBar.swift
+++ MobileDevelopmentIOS/MobileDevelopmentIOS/Components/Organisms/TopBar.swift
@@ -5,3 +5,15 @@
 //  Created by Student on 23/03/2026.
 //
 
+enum FFTab {
+    case home, detector , game , history, profile
+}
+
+struct TopBar : View {
+    var body : some View {
+        VStack(spacing: 20) {
+          
+        }
+        .background(Color.ff)
+    }
+}
